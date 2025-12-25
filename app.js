2c8b1812b473151cb3ac301f95de25e127626c4b
Abhishek Raj <abhi@raj.me>
2025-12-25 21:24:25 +0700

    Add helloAuth

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var helloConfig = require('./hello.config.js');
+var hello = require('@hellocoop/express');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,8 +21,11 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.text());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(hello.helloAuth(helloConfig));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ hello.config.js
@@ -0,0 +1,4 @@
+const config = {
+    client_id: 'app_GJ7sOftv43u5OwpXWbEPnU69_4gC'
+}
+module.exports = config
