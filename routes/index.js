0353f85f168b2b7cb2da4840a1b4f8a40ed43c8a
Abhishek Raj <abhi@raj.me>
2025-12-26 00:09:20 +0700

    check auth status

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  await req.getAuth();
+  res.render('index', { title: 'Express', auth: JSON.stringify(req.auth) });
 });
 
 module.exports = router;
